f593dfb596e414b0466c6d544dc061f799845ce8
Justin Plock <jplock@smoketurner.com>
2018-08-21 22:03:33 -0400

    Ensure input streams are closed

--- src/main/java/com/smoketurner/dropwizard/riak/RiakFactory.java
+++ src/main/java/com/smoketurner/dropwizard/riak/RiakFactory.java
@@ -15,17 +15,6 @@
  */
 package com.smoketurner.dropwizard.riak;
 
-import com.basho.riak.client.api.RiakClient;
-import com.basho.riak.client.core.RiakCluster;
-import com.basho.riak.client.core.RiakNode;
-import com.basho.riak.client.core.util.DefaultCharset;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Strings;
-import com.google.common.net.HostAndPort;
-import com.google.common.primitives.Ints;
-import io.dropwizard.util.Duration;
-import io.dropwizard.validation.MinDuration;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -41,6 +30,17 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.jetbrains.annotations.Nullable;
+import com.basho.riak.client.api.RiakClient;
+import com.basho.riak.client.core.RiakCluster;
+import com.basho.riak.client.core.RiakNode;
+import com.basho.riak.client.core.util.DefaultCharset;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Strings;
+import com.google.common.net.HostAndPort;
+import com.google.common.primitives.Ints;
+import io.dropwizard.util.Duration;
+import io.dropwizard.validation.MinDuration;
 
 public class RiakFactory {
 
@@ -281,6 +281,7 @@ public class RiakFactory {
     try (InputStream inputStream = new FileInputStream(keyStorePath)) {
       final CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
       final X509Certificate cert = (X509Certificate) certFactory.generateCertificate(inputStream);
+      inputStream.close();
 
       keyStore = KeyStore.getInstance(keyStoreType);
       if (!Strings.isNullOrEmpty(keyStorePassword)) {
@@ -301,6 +302,7 @@ public class RiakFactory {
     try (InputStream inputStream = new FileInputStream(trustStorePath)) {
       final CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
       final X509Certificate caCert = (X509Certificate) certFactory.generateCertificate(inputStream);
+      inputStream.close();
 
       trustStore = KeyStore.getInstance(trustStoreType);
       if (!Strings.isNullOrEmpty(trustStorePassword)) {
